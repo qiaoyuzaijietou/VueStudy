56a55e1656af1c39d7189b44fe7337f52523e57e
qiaoyu <1903293766@qq.com>
2022-04-19 22:47:46 +0800

    4.19

--- 16_src_todoList_nextTick/components/MyList.vue
+++ 16_src_todoList_nextTick/components/MyList.vue
@@ -1,12 +1,10 @@
 <template>
   <ul class="todo-main">
-    <transition-group name="todo" appear="">
     <MyItem 
       v-for="todoObj in todos" 
       :key="todoObj.id" 
       :todo="todoObj" 
       />
-    </transition-group>
   </ul>
 </template>
 
@@ -35,19 +33,5 @@ export default {
   border-radius: 2px;
   padding-left: 5px;
   margin-top: 10px;
-}
-.todo-enter-active {
-  animation: atguigu 0.5s linear;
-}
-.todo-leave-active {
-  animation: atguigu 0.5s linear reverse;
-}
-@keyframes atguigu {
-  from {
-    transform: translateX(100%);
-  }
-  to {
-    transform: translateX(0px);
-  }
 }
 </style>
